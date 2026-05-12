c81a111162adda7f956793261646e92d1e2ede18
Genesis Visbal <sarayvisbal18@gmail.com>
2026-05-12 17:14:51 -0500

    Corregir sistema 5S pro completo

--- app.py
+++ app.py
@@ -246,23 +246,26 @@ if "color_secundario" not in st.session_state:
 if "color_alerta" not in st.session_state:
     st.session_state.color_alerta = "#D53333"
 
-st.sidebar.markdown("---")
-st.sidebar.subheader("🎨 Colores gráficas")
+with st.expander("🎨 Personalizar colores del dashboard", expanded=False):
+    c1, c2, c3 = st.columns(3)
 
-st.session_state.color_primario = st.sidebar.color_picker(
-    "Color principal",
-    st.session_state.color_primario
-)
+    with c1:
+        st.session_state.color_primario = st.color_picker(
+            "Color principal",
+            st.session_state.color_primario
+        )
 
-st.session_state.color_secundario = st.sidebar.color_picker(
-    "Color secundario",
-    st.session_state.color_secundario
-)
+    with c2:
+        st.session_state.color_secundario = st.color_picker(
+            "Color secundario",
+            st.session_state.color_secundario
+        )
 
-st.session_state.color_alerta = st.sidebar.color_picker(
-    "Color alerta/meta",
-    st.session_state.color_alerta
-)
+    with c3:
+        st.session_state.color_alerta = st.color_picker(
+            "Color alerta/meta",
+            st.session_state.color_alerta
+        )
 
 PALETA_VIVA = [
     st.session_state.color_primario,
